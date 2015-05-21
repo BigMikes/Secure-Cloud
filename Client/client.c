fb8990d5127f397fb858eb0ba3a5f1a02c9400b6
luigiDB <luigi.debianchi@gmail.com>
2015-05-22 01:23:55 +0200

    primo messaggio inviato dal client ( con errori legati alla secure_write )

--- Client/client.c
+++ Client/client.c
@@ -1,8 +1,15 @@
 #include "../security_ssl.h"
+#include "../utils.h"
+#include "upload.c"
+#include "download.c"
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <stdio.h>
 #include <string.h>
+
+//costanti
+#define DIM_BUFFER 1024
+#define DIM_USER_FIELD 50
 
 int create_socket(char* address, int port){
 	int ret;
@@ -28,6 +35,13 @@ int create_socket(char* address, int port){
 	return lst_sk;
 }
 
+void check_ret(int a, int b){
+	if(a != b)
+		exit (EXIT_FAILURE);
+}
+
+
+
 //accetta in ingresso indirizzo ip e porta
 int main(int argc,char* argv[]){
 	int ret;
@@ -37,6 +51,22 @@ int main(int argc,char* argv[]){
 	char* certif_path;
 	SSL_CTX* ssl_factory;
 	SSL* connection;
+
+	
+	//user data
+	char username[DIM_USER_FIELD];
+	int dim_username;
+	char password[DIM_USER_FIELD];
+	int dim_password;
+	char support;
+	uint8_t command;
+	char file_name[DIM_USER_FIELD];
+	int dim_file_name;
+	
+	//server response
+	uint8_t server_response;
+	
+
 	
 	/////////////////////////////////////////////////////////////////error check
 	if(argc < 3) {
@@ -70,17 +100,75 @@ int main(int argc,char* argv[]){
 		exit(-1);
 	}
 	
-	/*
-	iniziare la connessione con la secure_write
-	*/
-	
 	ret = client_connect(connection);
 	if(ret != 1){
 		exit(-1);
 	}
 	printf("Connection succefully\n");
 	
-	secure_write(0, "Ciao\0", 5, connection); 
+	//secure_write(0, "Ciao\0", 5, connection); 
+	////////////////////////////////////////////////////////////////////////body
+	printf("Username:\n");
+	dim_username = scanf("%s", username);
+	if( dim_username == 0){
+		printf("Error");
+		exit(-1);
+	}
+	
+	printf("\nPassword:\n");
+	dim_password = scanf("%s", password);
+	if( dim_password == 0){
+		printf("Error");
+		exit(-1);
+	}
+	
+	printf("\n Operation Upload or Download (u/d):\n");
+	scanf("%c", &support);
+	switch(support){
+		case 'u':
+		case 'U':
+			command = 1;
+			break;
+		case 'd':
+		case 'D':
+			command = 2;
+			break;
+		default:
+			printf("Error");
+			exit(-1);
+	}
+	
+	//primo messaggio user || pwd || cmd || name_file
+	ret = secure_write(0, dim_username, 4, connection); 
+	check_ret(ret, 4);
+	ret = secure_write(0, username, dim_username, connection);
+	check_ret(ret, dim_username);
+	ret = secure_write(0, dim_password, 4, connection); 
+	check_ret(ret, 4);
+	ret = secure_write(0, password, dim_password, connection); 
+	check_ret(ret, dim_password);
+	ret = secure_write(0, command, 1, connection); 
+	check_ret(ret, 1);
+	ret = secure_write(0, dim_file_name, 4, connection); 
+	check_ret(ret, 4);
+	ret = secure_write(0, file_name, dim_file_name, connection); 
+	check_ret(ret, dim_file_name);
+	
+	//wait for response
+	ret = secure_read(0, &server_response, sizeof(uint8_t), connection);
+	check_ret(ret, sizeof(uint8_t));
+	if(server_response == 0){
+		printf("Connection refused.\n");
+		exit(-1);
+	}
+	
+	if(command == 1){
+		upload(connection, file_name, dim_file_name);
+	}else{
+		//download();
+	}
+	
+	
 	/////////////////////////////////////////////////////////////////////cleanup
 	if(-1 == close(socketS)){
 		printf("Error in closing operation.\n");
