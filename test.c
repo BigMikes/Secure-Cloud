ba1365485210f7f37db500b9981753a27a1b3963
luigiDB <spada.elfica@gmail.com>
2015-05-29 01:23:37 +0200

    test file per le funzioni di hash e cifratura simmetrica

--- test.c
+++ test.c
@@ -1,5 +1,11 @@
 #include "utils.h"
 
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <openssl/rand.h>
 
 void print_bytes(unsigned char* buf, int len) {
   int i = 0;
@@ -10,21 +16,56 @@ void print_bytes(unsigned char* buf, int len) {
 }
 
 int main(){
-	unsigned char* hash_val;
-	unsigned char* buf = "stringa di prova";
-	char* name_file = "./VIDEO0015.mp4";
-	int ret;
 	
-	hash_val = do_hash(buf, strlen(buf), name_file);
+	unsigned char* hash_val;
+	char* name_file = "utils.c";
+	int ret;
+	unsigned char* cipher;
+	int cipher_len;
+	
+	//chiavi
+	//symmetric cipher
+		unsigned char* key;
+		int key_len;
+		
+		//generate k
+		key_len = EVP_CIPHER_key_length(SYM_CIPHER);
+		key = (unsigned char*)malloc(key_len);
+		RAND_seed(key, key_len);
+		RAND_bytes(key, key_len);
+	////
+	
+	hash_val = do_hash(name_file);
 	
 	printf("Hash Value = \n");
 	print_bytes(hash_val, EVP_MD_size(EVP_sha256()));
 	
-	ret = verify_hash(buf, strlen(buf), name_file, hash_val);
+	ret = verify_hash(name_file, hash_val);
 	if(ret == 1)
-		printf("Integrity verified\n");
+		printf("Integrity verified\n\n");
 	else
-		printf("Integrity check failed\n");
+		printf("Integrity check failed\n\n");
+	
+	//file encrypt
+	cipher = sym_crypto_file(name_file, hash_val, EVP_MD_size(EVP_sha256()), key, &cipher_len);
+	
+	struct stat st;
+	stat(name_file, &st); //file_size = st.st_size;
+	printf("test_file long: %i\n", (int)st.st_size);
+	printf("test_cipher_len: %i\n\n\n", cipher_len);
+	
+	//string encrypt
+	char* a = "ciao come va";
+	int l = strlen(a);
+	printf("stringa: %s\t\t lunga: %i\n", a, l);
+	cipher = sym_crypt(a, l, key, &cipher_len);
+	
+	printf("cipher: %s\t\t lungo: %i\n\n", cipher, cipher_len);
+	
+	//string decrypt
+	int plain_len;
+	char* plain = sym_decrypt(cipher, cipher_len, key, &plain_len);
+	printf("plain text: %s\t\t lungo: %i\n", plain, plain_len);
 	
 	return 0;	
 }
