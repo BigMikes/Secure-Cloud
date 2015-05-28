2552cc54b089f793759a4afce511aa91dabaaf7d
luigiDB <spada.elfica@gmail.com>
2015-05-29 01:31:55 +0200

    Aggiornamento del file h

--- utils.h
+++ utils.h
@@ -6,11 +6,15 @@
 
 #define SYM_CIPHER EVP_aes_256_cbc()
 
-unsigned char* do_hash(unsigned char* source, int source_size, char* name_file);
+unsigned char* do_hash(char* name_file);
 
-int verify_hash(unsigned char* source, int source_size, char* name_file ,unsigned char* hash_val);
+int verify_hash(char* name_file ,unsigned char* hash_val);
 
-unsigned char* sym_crypto(unsigned char* source, int source_size, char* name_file, unsigned char* key);
+unsigned char* sym_crypto_file(char* name_file, unsigned char* hash, int hash_size, unsigned char* key, int* cipher_len);
+
+unsigned char* sym_crypt(char* buf, int buf_size, unsigned char* key, int* cipher_len);
+
+char* sym_decrypt(unsigned char* cipher, int cipher_len, unsigned char* key, int* output_len);
 
 unsigned char* asym_crypto(unsigned char* plaintext, int in_len, int* out_len, char* pub_key_file);
 
