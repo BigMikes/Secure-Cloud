1d932c4684384347887db79736dfdcb573a1eb94
luigiDB <spada.elfica@gmail.com>
2015-05-29 01:31:40 +0200

    Aggiunte funzioni per la cifratura simmetrica e modifica dei parametri della hash function

--- utils.c
+++ utils.c
@@ -4,38 +4,34 @@
 * Computes the hash function SHA256 on "source" buffer, AND on the content of file "name_file" if it is non-NULL
 * Returns the hash value, NULL in case of errors
 */
-unsigned char* do_hash(unsigned char* source, int source_size, char* name_file){
+unsigned char* do_hash(char* name_file){
 	int i;
 	unsigned char data[1024];
-	unsigned int digest_size = EVP_MD_size(EVP_sha256());	
+	unsigned int digest_size = EVP_MD_size(HASH_FUN);	
 	unsigned char* hash = malloc(digest_size);
 	int read_bytes;
 	FILE* input_file;
+	EVP_MD_CTX sha_ctx;
 	
-	if(source == NULL || source_size <= 0){
+	if(name_file == NULL){
 		return NULL;
 	}
 	
-	EVP_MD_CTX sha_ctx;
 	EVP_MD_CTX_init(&sha_ctx);
 	
-	EVP_DigestInit(&sha_ctx, EVP_sha256());
+	EVP_DigestInit(&sha_ctx, HASH_FUN);
 	
-	EVP_DigestUpdate(&sha_ctx, source, source_size);
-	
-	if(name_file != NULL){
-		input_file = fopen(name_file, "r");
-		if(input_file==NULL){
-			printf("Impossible to open file.\n");
-			return NULL;
-		}
-		
-		while( ( read_bytes = fread(data, 1, 1024, input_file) ) != 0 ){
-			EVP_DigestUpdate(&sha_ctx, data, read_bytes);
-		}
-		
-		fclose(input_file);
+	input_file = fopen(name_file, "r");
+	if(input_file==NULL){
+		printf("Impossible to open file.\n");
+		return NULL;
 	}
+	
+	while( ( read_bytes = fread(data, 1, 1024, input_file) ) != 0 ){
+		EVP_DigestUpdate(&sha_ctx, data, read_bytes);
+	}
+	
+	fclose(input_file);
 	
 	EVP_DigestFinal(&sha_ctx, hash, &digest_size);
 	
@@ -54,19 +50,19 @@ unsigned char* do_hash(unsigned char* source, int source_size, char* name_file){
 * then compares it with the content of "hash_val" buffer
 * Returns 1 if the integrity is verified, 0 otherwise, -1 in case of errors
 */
-int verify_hash(unsigned char* source, int source_size, char* name_file ,unsigned char* hash_val){
+int verify_hash(char* name_file ,unsigned char* hash_val){
 	unsigned char* computed_hash;
 	int ret;
 	
 	/*Checks*/
-	if(source == NULL || hash_val == NULL || source_size <= 0)
+	if(hash_val == NULL || name_file == NULL)
 		return -1;
 		
 	/*Computes the hash function on source buffer and on file (if needed)*/
-	computed_hash = do_hash(source, source_size, name_file);
+	computed_hash = do_hash(name_file);
 	
 	/*Checks if they are equals*/
-	ret = CRYPTO_memcmp(computed_hash, hash_val, EVP_MD_size(EVP_sha256()));
+	ret = CRYPTO_memcmp(computed_hash, hash_val, EVP_MD_size(HASH_FUN));
 	
 	free(computed_hash);
 	
@@ -81,37 +77,174 @@ int verify_hash(unsigned char* source, int source_size, char* name_file ,unsigne
 * It is an error if "source" and "name_file" are both equal to NULL.
 * Returns the ciphertext, NULL in case of errors
 */
-unsigned char* sym_crypto_file(unsigned char* source, int source_size, char* name_file, unsigned char* key){
+unsigned char* sym_crypto_file(char* name_file, unsigned char* hash, int hash_size, unsigned char* key, int* cipher_len){
 	EVP_CIPHER_CTX* ctx;
 	FILE* fd;
 	unsigned char* ciphertext;
 	unsigned char* plaintext;
+	int res, msg_len, n;
+	int outlen, outlen_tot;
+	int ct_bufsize;
+	int block_size = EVP_CIPHER_block_size(SYM_CIPHER);
 	
-	if((source == NULL && name_file == NULL) || key == NULL)
+	//printf("block_size: %i\n", block_size);
+	
+	if(hash == NULL || name_file == NULL || key == NULL)
 		return NULL;
 	
-	if(name_file != NULL){
-		fd = fopen(name_file, "r");
-		if(fd == NULL){
-			printf("Impossible to open %s file\n", name_file);
-			return NULL;
-		}
+	fd = fopen(name_file, "r");
+	if(fd == NULL){
+		printf("Impossible to open %s file\n", name_file);
+		return NULL;
 	}
+	
+	struct stat st;
+	stat(name_file, &st); //file_size = st.st_size;
+	plaintext = (unsigned char*)malloc(st.st_size+hash_size);		//plaintext grande come il file più hash
 	
 	ctx = (EVP_CIPHER_CTX*)malloc(sizeof(EVP_CIPHER_CTX));	
 	EVP_CIPHER_CTX_init(ctx);
 	EVP_EncryptInit(ctx, SYM_CIPHER, key, NULL);		//Cosa fare con l' IV?
 	
-	//EVP_EncryptUpdate();
-	//EVP_EncryptFinal();
+	//plaintext allocation
+	res=fread(plaintext, sizeof(char), st.st_size, fd); // in res ci sono i byte letti
+	if(res != st.st_size){
+		printf("reading error");
+		return NULL;
+	}
 	
+	//append hash
+	memcpy(plaintext + st.st_size, hash, hash_size);
+	
+	/* Buffer allocation for the ciphertext */
+	msg_len = st.st_size+hash_size;
+	ct_bufsize = msg_len + block_size;
+	ciphertext = (unsigned char*)malloc(ct_bufsize);
+	
+	
+	
+	outlen = 0;
+	outlen_tot = 0;//dimensione testo output(ciphertext)
+	n=0;//dimensione testo input (plaintext)
+	while(n/block_size < msg_len/block_size){//block size serve nel caso in cui msg_len < block_size allora non devo entrare nel ciclo
+		EVP_EncryptUpdate(ctx, ciphertext + outlen_tot, &outlen, (unsigned char*)plaintext + n,block_size);
+		outlen_tot += outlen;
+		n += block_size;
+	}
+	EVP_EncryptUpdate(ctx, ciphertext + outlen_tot, &outlen, (unsigned char*)plaintext + n,msg_len % block_size);// cifro i byte restanti(quelli non multipli di block size
+	outlen_tot += outlen;
+	n += msg_len % block_size;
+	EVP_EncryptFinal(ctx, ciphertext + outlen_tot, &outlen);
+	outlen_tot += outlen;
 	
 	EVP_CIPHER_CTX_cleanup(ctx);
+	
 	free(ctx);
+	
+	*cipher_len = outlen_tot;
+	printf("ciphertext long: %i\n", outlen_tot);
+	return ciphertext;
+}
+
+/*
+ * 
+ */
+unsigned char* sym_crypt(char* buf, int buf_size, unsigned char* key, int* cipher_len){
+	EVP_CIPHER_CTX* ctx;
+	FILE* fd;
+	unsigned char* ciphertext;
+	int res, msg_len, n;
+	int outlen, outlen_tot;
+	int ct_bufsize;
+	int block_size = EVP_CIPHER_block_size(SYM_CIPHER);
+	
+	//printf("block_size: %i\n", block_size);
+	
+	if(buf == NULL || key == NULL)
+		return NULL;
+	
+	
+	ctx = (EVP_CIPHER_CTX*)malloc(sizeof(EVP_CIPHER_CTX));	
+	EVP_CIPHER_CTX_init(ctx);
+	EVP_EncryptInit(ctx, SYM_CIPHER, key, NULL);		//Cosa fare con l' IV?
+	
+	
+	/* Buffer allocation for the ciphertext */
+	msg_len = buf_size;
+	ct_bufsize = msg_len + block_size;
+	ciphertext = (unsigned char*)malloc(ct_bufsize);
+	
+	
+	
+	outlen = 0;
+	outlen_tot = 0;//dimensione testo output(ciphertext)
+	n=0;//dimensione testo input (plaintext)
+	while(n/block_size < msg_len/block_size){//block size serve nel caso in cui msg_len < block_size allora non devo entrare nel ciclo
+		EVP_EncryptUpdate(ctx, ciphertext + outlen_tot, &outlen, (unsigned char*)buf + n,block_size);
+		outlen_tot += outlen;
+		n += block_size;
+	}
+	EVP_EncryptUpdate(ctx, ciphertext + outlen_tot, &outlen, (unsigned char*)buf + n,msg_len % block_size);// cifro i byte restanti(quelli non multipli di block size
+	outlen_tot += outlen;
+	n += msg_len % block_size;
+	EVP_EncryptFinal(ctx, ciphertext + outlen_tot, &outlen);
+	outlen_tot += outlen;
+	
+	EVP_CIPHER_CTX_cleanup(ctx);
+	
+	free(ctx);
+	
+	*cipher_len = outlen_tot;
+	return ciphertext;
 }
 
 
-unsigned char* sym_decrypto_file(){}
+/*
+ * NB: questa funzione restituisce un buffer più grande del dovuto per cui 
+ * fare sempre riferimento al valore di output_len
+ */
+char* sym_decrypt(unsigned char* cipher, int cipher_len, unsigned char* key, int* output_len){
+	char* plaintext;
+	int outlen, outlen_tot, n, len, res;
+	int block_size = EVP_CIPHER_block_size(SYM_CIPHER);
+	
+	if(cipher == NULL || key == NULL){
+		return NULL;
+	}
+	
+	plaintext = (char*)malloc(cipher_len + block_size);
+	
+	EVP_CIPHER_CTX* ctx = (EVP_CIPHER_CTX*)malloc(sizeof(EVP_CIPHER_CTX));
+	EVP_CIPHER_CTX_init(ctx);
+	EVP_DecryptInit(ctx, SYM_CIPHER, key, NULL);
+	
+	len = cipher_len;	//passaggio inutile ma serve per mantenere una continuità con le funzioni precedenti
+	/* Decryption */
+	outlen = 0;
+	outlen_tot = 0;// dim testo output (plaintext)
+	n=0;//dim testo input (ciphertext)
+	while(n/block_size < len/block_size){
+		EVP_DecryptUpdate(ctx, plaintext+ outlen_tot, &outlen, (char*)cipher+ n, block_size);
+		outlen_tot += outlen;
+		n += block_size;
+	}
+	EVP_DecryptUpdate(ctx, plaintext+ outlen_tot, &outlen, (char*)cipher+ n, len % block_size);
+	outlen_tot += outlen;
+	n += len % block_size;
+	res = EVP_DecryptFinal(ctx, plaintext + outlen_tot, &outlen);
+	if(res == 0){
+		printf("ERROR in decrypting.\n");
+		return NULL;
+	}
+	outlen_tot += outlen;
+	
+	
+	EVP_CIPHER_CTX_cleanup(ctx);
+	
+	*output_len = outlen_tot;
+	return plaintext;
+	
+}
 
 
 
